1f237fe4711ff7c737d0c01c4e1454fad8ddde30
John Yang <byjohnyang@gmail.com>
2017-04-28 05:26:12 -0700

    numpy file

---
+++ numpy-playground.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+###########################
+# Numpy Array Exploration #
+###########################
+
+# Reference List:
+# 1. https://datax911.files.wordpress.com/2016/12/numpy-v1-12.pdf
+
+print("Matrix")
+a = np.arange(15).reshape(3, 5)
+print("Shape, N-Dimensional, Data Type, Size")
+print(a.shape, " ", a.ndim, " ", a.dtype.name, " ", a.size)
+b = np.array([6, 7, 8])
+c = np.array([(1, 2, 3), (4, 5, 6)])
+zeroes = np.zeroes((3, 4))
+ones = np.ones((5, 6))
