53172399e13320e95f6e88427d02958cf7f0e1f2
John Yang <byjohnyang@gmail.com>
2017-06-08 09:14:39 -0700

    first batch of notes for today

--- Objective C/basics2.m
+++ Objective C/basics2.m
@@ -1,3 +1,5 @@
+#import <Foundation/Foundation.h>
+
 //NUMBERS
 @interface sampleClass:NSObject
 - (NSNumber *) raisePower: (NSNumber *)a toExponent:(NSNumber *) b;
@@ -19,6 +21,27 @@ extern array2[] = {1, 2, 3, 4, 5};
 extern array3[5] = {5, 4, 3, 2, 1};
 
 //POINTERS
+extern int original = 20;
+extern int *reference;
+reference = &original;
+
+//STRUCTURES
+struct books {
+    NSString *title;
+    NSString *author;
+    int publication;
+}
+
+@interface bookClass:NSObject
+- (void) printBook: (struct books) book;
+@end
+
+@implementation bookClass
+- (void) printBook: (struct books) book {
+    NSLog(@"Title of the book: %@\n", book.title);
+    NSLog(@"Publication fo the book: %d\n", book.publication);
+}
+@end
 
 float main() {
     //NUMBERS
@@ -29,6 +52,26 @@ float main() {
 
     //ARRAYS
     NSLog(@"4th Value of array: %d", array3[3]);
+
+    //STRINGS
+    NSString *str1 = "Hello";
+    NSString *str2 = "World";
+    NSString *str3;
+    int len;
+
+    str3 = [str uppercaseString]; //Uppercase
+    str3 = [str1 stringByAppendingFormat:@" People"]; //Concatenation
+    len = [str3 length]; //Length of String
+    str3 = [[NSString alloc] initWithFormat:@"%@ %@", str1, str2]; //Initialized with format
+
+    //STRUCTURES
+    struct books greatGatsby;
+    greatGatsby.title = @"Great Gatsby";
+    greatGatsby.author = @"Scott Fitzgerald";
+    greatGatsby.publication = 1945;
+
+    bookClass *newBook = [[bookClass alloc] init];
+    [newBook printBook: greatGatsby];
 
     return [result floatValue];
 }
